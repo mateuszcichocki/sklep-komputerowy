50c7732910935d02c83998335114ad992b058174
mateusz <mateusz.cichocki@doradigital.pl>
2018-04-20 13:46:42 +0200

    Create project

---
+++ src/Sklep/AdminBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sklep\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('@SklepAdmin/Default/index.html.twig');
+    }
+}
---
+++ src/Sklep/AdminBundle/SklepAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sklep\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SklepAdminBundle extends Bundle
+{
+}
